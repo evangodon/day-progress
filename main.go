9664f5cb1c45fc3212d7d0b65c80c33c07548954
Evan Godon <e.godon@outlook.com>
2023-02-21 16:35:07 -0500

    tidy imports

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/muesli/termenv"
 )
 
@@ -12,18 +14,20 @@ const (
 	colorOne = "#FF7CCB"
 	colorTwo = "#FDFF8C"
 	char     = '█'
+	width    = 100
 )
 
 var now = time.Now()
 var dayStart = time.Date(now.Year(), now.Month(), now.Day(), 9, 0, 0, 0, time.Local)
 var dayEnd = time.Date(now.Year(), now.Month(), now.Day(), 17, 0, 0, 0, time.Local)
 
+// TODO: flags to set color
 func main() {
 	prog := progress.New(
 		progress.WithScaledGradient(colorOne, colorTwo),
 		progress.WithColorProfile(termenv.TrueColor),
 	)
-	prog.Width = 100
+	prog.Width = width
 	prog.ShowPercentage = false
 	prog.Full = char
 	prog.Empty = char
@@ -31,7 +35,13 @@ func main() {
 
 	start := dayStart.Format(time.Kitchen)
 	end := dayEnd.Format(time.Kitchen)
-	fmt.Printf("%s %s %s", start, prog.ViewAs(percentage), end)
+
+	text := fmt.Sprintf("%.0f%% of day done", math.Round(percentage*100))
+	title := lipgloss.NewStyle().Width(width + len(start) + len(end)).Align(lipgloss.Center).Faint(true)
+	fmt.Print(title.Render(text))
+
+	fmt.Printf("\n%s %s %s", start, prog.ViewAs(percentage), end)
+
 }
 
 func dayProgress() float64 {
