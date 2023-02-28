9b9b8685726b00f4c6a57fa1f68b442805361c02
Evan Godon <e.godon@outlook.com>
2023-02-28 12:39:41 -0500

    feat: add flags to set start and end times

--- main.go
+++ main.go
@@ -19,14 +19,19 @@ const (
 )
 
 var now = time.Now()
-var dayStart = time.Date(now.Year(), now.Month(), now.Day(), 9, 0, 0, 0, time.Local)
-var dayEnd = time.Date(now.Year(), now.Month(), now.Day(), 17, 0, 0, 0, time.Local)
+var dayStart = "9:30AM"
+var dayEnd = "5:30PM"
 
 func main() {
 	colorLeft := flag.String("color-1", colorOne, "Left color")
 	colorRight := flag.String("color-2", colorTwo, "Right color")
 	width := flag.Int("width", defaultWidth, "width")
+	start := flag.String("start", dayStart, "start time")
+	end := flag.String("end", dayEnd, "end time")
 	flag.Parse()
+
+	dayStart := parseTimeFlag(*start)
+	dayEnd := parseTimeFlag(*end)
 
 	prog := progress.New(
 		progress.WithScaledGradient(*colorLeft, *colorRight),
@@ -36,10 +41,10 @@ func main() {
 	prog.ShowPercentage = false
 	prog.Full = char
 	prog.Empty = char
-	percentage := dayProgress()
+	percentage := dayProgress(dayStart, dayEnd)
 
-	start := dayStart.Format(time.Kitchen)
-	end := dayEnd.Format(time.Kitchen)
+	startKitchen := dayStart.Format(time.Kitchen)
+	endKitchen := dayEnd.Format(time.Kitchen)
 
 	s := math.Round(percentage * 100)
 	if s > 100 {
@@ -47,17 +52,29 @@ func main() {
 	}
 
 	text := fmt.Sprintf("%.0f%% of day done", s)
-	title := lipgloss.NewStyle().Width(defaultWidth + len(start) + len(end)).Align(lipgloss.Center).Faint(true)
+	title := lipgloss.NewStyle().Width(defaultWidth + len(startKitchen) + len(endKitchen)).Align(lipgloss.Center).Faint(true)
 	fmt.Print(title.Render(text))
 
-	fmt.Printf("\n%s %s %s", start, prog.ViewAs(percentage), end)
+	fmt.Printf("\n%s %s %s", startKitchen, prog.ViewAs(percentage), endKitchen)
 
 }
 
-func dayProgress() float64 {
+func parseTimeFlag(str string) time.Time {
+	layout := time.Kitchen
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		fmt.Printf("Could not parse time %s\nError: %v\n", str, err)
+	}
 
-	totalTimeRange := dayEnd.Sub(dayStart).Minutes()
-	sinceDayStart := now.Sub(dayStart).Minutes()
+	return t
+}
+
+func dayProgress(s time.Time, e time.Time) float64 {
+	start := time.Date(now.Year(), now.Month(), now.Day(), s.Hour(), s.Minute(), 0, 0, time.Local)
+	end := time.Date(now.Year(), now.Month(), now.Day(), e.Hour(), e.Minute(), 0, 0, time.Local)
+
+	totalTimeRange := end.Sub(start).Minutes()
+	sinceDayStart := now.Sub(start).Minutes()
 
 	percentage := sinceDayStart / totalTimeRange
 
